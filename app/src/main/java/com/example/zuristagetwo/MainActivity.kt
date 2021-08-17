e7a2c329bdc6658e3b946a4d4cc897b0bb74aa6c
tolulonge <tolulopelonge945@gmail.com>
2021-08-17 13:44:55 +0100

    pushing code

---
+++ app/src/main/java/com/example/zuristagetwo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.zuristagetwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.zuristagetwo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.activityMainDisplayBtn.setOnClickListener {
+            binding.activityMainTxtName.visibility = View.VISIBLE
+            val name = binding.activityMainEdt.text
+            if (name?.isNotEmpty() == true){
+                binding.activityMainTxtName.text = name
+            }else{
+                Snackbar.make(binding.root,"Please enter your name",Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/zuristagetwo/PrintMyName.kt
@@ -0,0 +1,6 @@
+package com.example.zuristagetwo
+
+fun main(){
+    val myName = "Tolulope Longe"
+    println(myName)
+}
