e059f4c726655edbc07db8c6a0efab53c87ea39d
tolulonge <tolulopelonge945@gmail.com>
2021-08-18 13:54:42 +0100

    displayed user's input on text changed

--- app/src/main/java/com/example/zuristagetwo/MainActivity.kt
+++ app/src/main/java/com/example/zuristagetwo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.zuristagetwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import com.example.zuristagetwo.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -13,15 +14,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.activityMainDisplayBtn.setOnClickListener {
-            binding.activityMainTxtName.visibility = View.VISIBLE
-            val name = binding.activityMainEdt.text
-            if (name?.isNotEmpty() == true){
-                binding.activityMainTxtName.text = name
-                binding.activityMainEdt.text?.clear()
-            }else{
-                Snackbar.make(binding.root,"Please enter your name",Snackbar.LENGTH_LONG).show()
-            }
+
+        binding.activityMainTxtName.visibility = View.VISIBLE
+        binding.activityMainEdt.doOnTextChanged { text, start, before, count ->
+            binding.activityMainTxtName.text = text
         }
 
     }
